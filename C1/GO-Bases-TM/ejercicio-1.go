7471f2f75c3b7b25189d610e5cf6e5a4d62a5fcc
nico-navarro <nicolas.navarro@mercadolibre.cl>
2022-09-23 12:43:17 -0300

    add ejercicio-1

---
+++ C1/GO-Bases-TM/ejercicio-1.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "Nicolás Navarro Tapia"
+	address := "Santiago, Chile"
+	fmt.Print("Mi nombre es ", name, " vivo en ", address)
+}
