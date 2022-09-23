03e0ad00f61321464f5c8521f9fddd15e849672c
nico-navarro <nicolas.navarro@mercadolibre.cl>
2022-09-23 12:56:13 -0300

    add ejercicio-2

---
+++ C1/GO-Bases-TM/ejercicio-2.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var temperature int
+	var humidity float64
+	var pressure int
+
+	temperature = 16
+	humidity = 0.54
+	pressure = 1017
+
+	fmt.Println("Temperatura:", temperature)
+	fmt.Println("Humedad:", humidity)
+	fmt.Println("Presión atmosférica:", pressure)
+}
