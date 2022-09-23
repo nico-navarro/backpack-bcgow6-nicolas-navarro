25830f37c45b8ebba9f7fb65637ed198919dd849
nico-navarro <nicolas.navarro@mercadolibre.cl>
2022-09-23 13:30:52 -0300

    add ejercicio-4

---
+++ C1/GO-Bases-TM/ejercicio-4.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	// var apellido string = "Gomez" -> Correcta
+	// var edad int = "35" -> Incorrecta, debía ser int el valor asignado
+	// boolean := "false"; -> Correcta
+	// var sueldo string = 45857.90 -> Incorrecta, se debió declarar como float
+	// var nombre string = "Julián" -> Correcta
+
+	var apellido string = "Gomez"
+	var edad int = 35
+	boolean := false
+	var sueldo float64 = 45857.90
+	var nombre string = "Julián"
+}
