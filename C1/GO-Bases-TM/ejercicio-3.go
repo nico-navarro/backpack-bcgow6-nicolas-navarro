88b8721274b4643022c310195a1cb1438ea203f0
nico-navarro <nicolas.navarro@mercadolibre.cl>
2022-09-23 13:17:11 -0300

    add ejercicio-3

---
+++ C1/GO-Bases-TM/ejercicio-3.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	// var 1nombre string -> incorrecta por comenzar con un número
+	// var apellido string -> correcta
+	// var int edad -> incorrecta está al revés
+	// 1apellido := 6 -> incorrecta por comenzar con número
+	// var licencia_de_conducir = true -> incorrecta por el uso de _
+	// var estatura de la persona int -> incorrecta por los espacios
+	// cantidadDeHijos := 2 -> correcta
+
+	var nombre string
+	var apellido string
+	var edad int
+	var licenciaDeConducir = true
+	var estaturaDeLaPersona int
+	cantidadDeHijos := 2
+}
