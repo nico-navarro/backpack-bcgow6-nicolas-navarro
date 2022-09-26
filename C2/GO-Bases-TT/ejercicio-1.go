3222819726d72ec98a067a0c2edffbf066467cd2
nico-navarro <nicolas.navarro@mercadolibre.cl>
2022-09-26 16:19:53 -0300

    add details method

--- C2/GO-Bases-TT/ejercicio-1.go
+++ C2/GO-Bases-TT/ejercicio-1.go
@@ -11,9 +11,17 @@ type Student struct {
 	// food     string // private
 }
 
+// esto es solo una función que recibe el struct, NO es un método
 func details(student Student) {
 	// Forma rápida de imprimir un struct
 	// fmt.Printf("Detalles: %+v\n", student)
+	fmt.Printf("%s %s\n", student.Name, student.Lastname)
+	fmt.Printf("- DNI: %s\n", student.DNI)
+	fmt.Printf("- Fecha: %s\n", student.Date)
+}
+
+// método
+func (student Student) detailsMethod() {
 	fmt.Printf("%s %s\n", student.Name, student.Lastname)
 	fmt.Printf("- DNI: %s\n", student.DNI)
 	fmt.Printf("- Fecha: %s\n", student.Date)
@@ -23,7 +31,11 @@ func main() {
 	nico := Student{
 		"Nicolas", "Navarro", "19231321-k", "21-12-1997",
 	}
+	//función que recibe un struct Student
 	details(nico)
+
+	//metodo
+	nico.detailsMethod()
 
 	// De esta forma puedo inicializar sin pasar todos los args
 	// gabi := Student{
