4d4801ca7eaadd09c70b0986f63b79d85b591be5
nico-navarro <nicolas.navarro@mercadolibre.cl>
2022-10-03 18:04:37 -0300

    add filter and GetOne user

--- 07-Go-Web/go-web/main.go
+++ 07-Go-Web/go-web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,30 +13,69 @@ type User struct {
 	Name   string
 	Email  string
 	Age    int
-	Height string
+	Height int
 	Active bool
 	Date   string
 }
 
-type Users struct {
-	Users []User
-}
-
 func GetAll(c *gin.Context) {
 	file, _ := ioutil.ReadFile("users.json")
-
 	users := []User{}
-
 	_ = json.Unmarshal([]byte(file), &users)
 
-	c.JSON(200, users)
+	result := []User{}
+
+	name := c.Query("name")
+	email := c.Query("email")
+	age, _ := strconv.Atoi(c.Query("age"))
+
+	for _, user := range users {
+
+		conditionName := true
+		if name != "" {
+			conditionName = user.Name == name
+		}
+
+		conditionEmail := true
+		if email != "" {
+			conditionEmail = user.Email == email
+		}
+
+		conditionAge := true
+		if age > 0 {
+			conditionAge = user.Age == age
+		}
+
+		if conditionName && conditionEmail && conditionAge {
+			result = append(result, user)
+		}
+	}
+	c.JSON(200, result)
+}
+
+func GetOne(c *gin.Context) {
+	file, _ := ioutil.ReadFile("users.json")
+	users := []User{}
+	_ = json.Unmarshal([]byte(file), &users)
+
+	userWanted := User{}
+	status := 404
+	id, _ := strconv.Atoi(c.Param("id"))
+	for _, user := range users {
+		if user.Id == id {
+			userWanted = user
+			status = 200
+			break
+		}
+	}
+	c.JSON(status, userWanted)
 }
 
 func main() {
 	// ROUTER
 	router := gin.Default()
 
-	// HANDLER
+	// example
 	router.GET("/hello", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hola Nico!",
@@ -44,6 +84,9 @@ func main() {
 
 	// Get All Users
 	router.GET("/users", GetAll)
+
+	// Get One User
+	router.GET("/users/:id", GetOne)
 
 	// Corremos nuestro servidor sobre el puerto 8080
 	router.Run()
