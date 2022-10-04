8a17f98fb1457241bab0ddac15fab94bb6e6e613
nico-navarro <nicolas.navarro@mercadolibre.cl>
2022-10-04 12:00:42 -0300

    add token validation

--- 07-Go-Web/C2/go-web/main.go
+++ 07-Go-Web/C2/go-web/main.go
@@ -18,18 +18,29 @@ var users []User = []User{}
 
 func Create(c *gin.Context) {
 	var user User
+
+	token := c.GetHeader("token")
+	if token != "123456" {
+		c.JSON(401, gin.H{
+			"error": "no tiene permisos para realizar la petición solicitada",
+		})
+		return
+	}
+
 	if err := c.ShouldBindJSON(&user); err != nil {
 		c.JSON(400, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
+
 	if len(users) > 0 {
 		user.Id = users[len(users)-1].Id + 1
 	} else {
 		user.Id = 1
 	}
 	users = append(users, user)
+
 	c.JSON(200, user)
 }
 
