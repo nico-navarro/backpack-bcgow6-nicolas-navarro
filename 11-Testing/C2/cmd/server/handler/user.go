6a509744edd21ae8631112e1e24f0d5691ac816b
nico-navarro <nicolas.navarro@mercadolibre.cl>
2022-10-20 11:42:30 -0300

    add Testing C2 TM files

---
+++ 11-Testing/C2/cmd/server/handler/user.go
@@ -0,0 +1,139 @@
+package handler
+
+import (
+	"C2/internal/users"
+	"C2/pkg/web"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type request struct {
+	Name   string `json:"name" binding:"required"`
+	Email  string `binding:"required"`
+	Age    *int   `binding:"required"`
+	Height *int   `binding:"required"`
+	Active *bool  `binding:"required"`
+	Date   string `binding:"required"`
+}
+
+type patchRequest struct {
+	Name   string
+	Email  string
+	Age    *int
+	Height *int
+	Active *bool
+	Date   string
+}
+
+type UserController struct {
+	service users.Service
+}
+
+func NewUserController(u users.Service) *UserController {
+	return &UserController{
+		service: u,
+	}
+}
+
+// ListUsers godoc
+// @Summary List users
+// @Tags Users
+// @Description get users
+// @Produce  json
+// @Param token header string true "token"
+// @Success 200 {object} web.Response
+// @Router /users [get]
+func (c *UserController) GetAll(ctx *gin.Context) {
+	token := ctx.Request.Header.Get("token")
+	if token != os.Getenv("TOKEN") {
+		ctx.JSON(401, web.NewResponse(401, nil, "token inválido"))
+		return
+	}
+
+	users, err := c.service.GetAll()
+	if err != nil {
+		ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
+		return
+	}
+	ctx.JSON(200, web.NewResponse(200, users, ""))
+}
+
+func (c *UserController) Store(ctx *gin.Context) {
+	token := ctx.Request.Header.Get("token")
+	if token != os.Getenv("TOKEN") {
+		ctx.JSON(401, web.NewResponse(401, nil, "token inválido"))
+		return
+	}
+	var req request
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
+		return
+	}
+	user, err := c.service.Store(req.Name, req.Email, *req.Age, *req.Height, *req.Active, req.Date)
+	if err != nil {
+		ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
+		return
+	}
+	ctx.JSON(200, web.NewResponse(200, user, ""))
+}
+
+func (c *UserController) Update(ctx *gin.Context) {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, "ID Not Found"))
+		return
+	}
+
+	var req request
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+		return
+	}
+
+	user, err := c.service.Update(id, req.Name, req.Email, *req.Age, *req.Height, *req.Active, req.Date)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, web.NewResponse(http.StatusNotFound, nil, err.Error()))
+		return
+	}
+	ctx.JSON(200, web.NewResponse(200, user, ""))
+}
+
+func (c *UserController) Delete(ctx *gin.Context) {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, "ID Not Found"))
+		return
+	}
+
+	user, err := c.service.Delete(id)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, web.NewResponse(http.StatusNotFound, nil, err.Error()))
+		return
+	}
+
+	ctx.JSON(200, web.NewResponse(200, user, ""))
+}
+
+func (c *UserController) Patch(ctx *gin.Context) {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, "ID Not Found"))
+		return
+	}
+
+	var req patchRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+		return
+	}
+
+	user, err := c.service.Patch(id, req.Name, req.Email, req.Age, req.Height, req.Active, req.Date)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, web.NewResponse(http.StatusNotFound, nil, err.Error()))
+		return
+	}
+	ctx.JSON(200, web.NewResponse(200, user, ""))
+}
---
+++ 11-Testing/C2/internal/users/service.go
@@ -0,0 +1,68 @@
+package users
+
+type Service interface {
+	GetAll() ([]User, error)
+	Store(name string, email string, age int, height int, active bool, date string) (User, error)
+	Update(id int, name string, email string, age int, height int, active bool, date string) (User, error)
+	Delete(id int) (User, error)
+	Patch(id int, name string, email string, age *int, height *int, active *bool, date string) (User, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(r Repository) Service {
+	return &service{
+		repository: r,
+	}
+}
+
+func (s *service) GetAll() ([]User, error) {
+	ps, err := s.repository.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return ps, nil
+}
+
+func (s *service) Store(name string, email string, age int, height int, active bool, date string) (User, error) {
+	lastID, err := s.repository.LastID()
+	if err != nil {
+		return User{}, err
+	}
+
+	lastID++
+
+	producto, err := s.repository.Store(lastID, name, email, age, height, active, date)
+	if err != nil {
+		return User{}, err
+	}
+
+	return producto, nil
+}
+
+func (s *service) Update(id int, name string, email string, age int, height int, active bool, date string) (User, error) {
+	user, err := s.repository.Update(id, name, email, age, height, active, date)
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
+
+func (s *service) Delete(id int) (User, error) {
+	user, err := s.repository.Delete(id)
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
+
+func (s *service) Patch(id int, name string, email string, age *int, height *int, active *bool, date string) (User, error) {
+	user, err := s.repository.Patch(id, name, email, age, height, active, date)
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
