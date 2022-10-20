caebad958ae98dcc26652aa6749b61ec49a3101c
nico-navarro <nicolas.navarro@mercadolibre.cl>
2022-10-20 15:26:16 -0300

    add updateTest

--- 11-Testing/C2/internal/users/repository_test.go
+++ 11-Testing/C2/internal/users/repository_test.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -62,6 +63,28 @@ func (s StubStore) Read(data interface{}) error {
 }
 
 func (s StubStore) Write(data interface{}) error {
+	stubData := []User{
+		{
+			Id:     1,
+			Name:   "Nicolas",
+			Email:  "nico@mercadolibre.cl",
+			Age:    1,
+			Height: 1293,
+			Active: true,
+			Date:   "25-11-2012",
+		},
+		{
+			Id:     2,
+			Name:   "Gabi",
+			Email:  "gabi@mercadolibre.cl",
+			Age:    1,
+			Height: 1293,
+			Active: true,
+			Date:   "25-11-2012",
+		},
+	}
+	stubDataB, _ := json.Marshal(stubData)
+	json.Unmarshal(stubDataB, data)
 	return nil
 }
 
@@ -91,6 +114,17 @@ func TestGetAll(t *testing.T) {
 	}
 	//act
 	resultado, _ := repo.GetAll()
+	//assert
+	assert.Equal(t, dataEsperada, resultado)
+}
+
+func TestUpdateName(t *testing.T) {
+	//arrange
+	myStubStore := StubStore{}
+	repo := NewRepository(myStubStore)
+	dataEsperada := User{1, "Nicolas", "nico@mercadolibre.cl", 1, 1293, true, "25-11-2012"}
+	//act
+	resultado, _ := repo.Update(1, "Nicolas", "nico@mercadolibre.cl", 1, 1293, true, "25-11-2012")
 	//assert
 	assert.Equal(t, dataEsperada, resultado)
 }
