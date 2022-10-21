31770ae8794aa527a40ad274ad94b3d233719762
nico-navarro <nicolas.navarro@mercadolibre.cl>
2022-10-20 22:46:56 -0300

    add service/repo integration delete test

--- 11-Testing/C2/internal/users/service_test.go
+++ 11-Testing/C2/internal/users/service_test.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -17,4 +18,39 @@ func TestServiceUpdate(t *testing.T) {
 	//assert
 	assert.Equal(t, dataEsperada, resultado)
 	assert.True(t, myStubStore.ReadWasCalled)
+}
+
+func TestServiceDelete(t *testing.T) {
+
+	myStubStore := StubStore{}
+	repo := NewRepository(&myStubStore)
+	service := NewService(repo)
+
+	// 1. Valida que user borrado efectivamente no exista
+	//	se debe modificar el Read() para que funcione bien el GetAll
+	// arrange
+	dataEsperada := []User{
+		{
+			Id:     1,
+			Name:   "Nico",
+			Email:  "nico@mercadolibre.cl",
+			Age:    1,
+			Height: 1293,
+			Active: true,
+			Date:   "25-11-2012",
+		},
+	}
+	//act
+	_, _ = service.Delete(1)
+	users, _ := service.GetAll()
+	//assert
+	assert.Equal(t, dataEsperada, users)
+
+	// 2. Obtiene el error correspondiente
+	//arrange
+	expectedError := errors.New("no existe el ID especificado")
+	//act
+	_, err := service.Delete(15)
+	//assert
+	assert.EqualError(t, err, expectedError.Error())
 }
