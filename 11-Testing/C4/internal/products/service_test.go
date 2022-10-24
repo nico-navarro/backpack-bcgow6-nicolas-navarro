43b7c1f410bb3962ad0a63fcb6fe7b113daeeb3d
nico-navarro <nicolas.navarro@mercadolibre.cl>
2022-10-24 11:25:26 -0300

    add service tests

--- 11-Testing/C4/internal/products/service_test.go
+++ 11-Testing/C4/internal/products/service_test.go
@@ -1,33 +1,48 @@
 package products
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 type StubRepo struct {
-	stubData interface{}
+	stubData  []Product
+	stubError error
 }
 
-func (s StubRepo) GetAllBySeller(sellerId string) ([]Product, error) {
-	return []Product{}, nil
+func (s *StubRepo) GetAllBySeller(sellerId string) ([]Product, error) {
+	if s.stubError != nil {
+		return []Product{}, s.stubError
+	}
+	return s.stubData, nil
 }
 
 func TestGetAllBySeller(t *testing.T) {
 	//arrange
 	dataEsperada := []Product{
 		{
-			ID:          "1",
-			SellerID:    "1",
-			Description: "a",
-			Price:       12,
+			ID:          "mock",
+			SellerID:    "FEX112AC",
+			Description: "generic product",
+			Price:       123.55,
 		},
 	}
 	myStubRepo := StubRepo{stubData: dataEsperada}
-	service := NewService(myStubRepo)
+	service := NewService(&myStubRepo)
 	//act
 	resultado, _ := service.GetAllBySeller("1")
 	//assert
 	assert.Equal(t, dataEsperada, resultado)
+}
+
+func TestGetAllBySellerFail(t *testing.T) {
+	//arrange
+	myStubRepo := StubRepo{stubError: errors.New("error in repository")}
+	service := NewService(&myStubRepo)
+	//act
+	_, err := service.GetAllBySeller("1")
+	//assert
+	assert.NotNil(t, err)
 }
